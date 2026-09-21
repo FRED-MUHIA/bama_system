1ed2c47e369e9e2f7591f99ac73b9330445db502
Your Name <you@example.com>
2026-09-22 01:16:04 +0300

    first commit

--- resources/views/layouts/marketing.blade.php
+++ resources/views/layouts/marketing.blade.php
@@ -105,7 +105,7 @@
             display: block;
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            object-fit: scale-down;
             object-position: center;
         }
         .industry-banner::before {
